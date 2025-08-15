72915acfc6689b35e388894afcf9b642b02d8eb8
Keith Ingram <101984509+truskoolcorp@users.noreply.github.com>
2025-08-14 20:02:26 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // src/main.js
-import * as THREE from '/vendor/three/build/three.module.js';
-import { OrbitControls } from '/vendor/three/examples/jsm/controls/OrbitControls.js';
-// import { GLTFLoader } from '/vendor/three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.159.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.159.0/examples/jsm/controls/OrbitControls.js';
+// import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.159.0/examples/jsm/loaders/GLTFLoader.js';
 
 // Grab canvas / renderer
 const canvas = document.getElementById('three');
